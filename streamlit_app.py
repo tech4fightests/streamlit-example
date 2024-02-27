53bb59de46bb2a60ef170eea19fbb2131615dea3
Caio C.R. Garcez <caio.garcez@redes.unb.br>
2024-02-27 16:52:15 -0300

    fix gitignore

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
+import base64
+from time import sleep
+
 import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-import base64
-from time import sleep
+
 
 def autoplay_audio(file_path: str):
     with open(file_path, "rb") as f:
@@ -17,7 +19,34 @@ def autoplay_audio(file_path: str):
         element = st.markdown(md, unsafe_allow_html=True)
         return element
         
+def play_stage(self, stage_idx: int, stage_period: str, total_number_of_kicks: int):
+    interval =  float(stage_period) / total_number_of_kicks
+
+    for i in range(total_number_of_kicks + 1):
+        info_holder = st.empty()
+        progress_holder = st.empty()
+
+        stage_holder, kick_holder, overall_kick_holder, kicks_per_minute_holder  = info_holder.columns([1,1,1,1])
+
+        progress = progress_holder.progress(0)
+        progress.progress(i * (1 / total_number_of_kicks))
+
+        if i < total_number_of_kicks:
+            stage_holder.metric("Stage", stage_idx)
+            kick_holder.metric("Kick", i)
+            overall_kick_holder.metric("Total Amount of Kicks", self.total_kicks)
+            kicks_per_minute_holder.metric("Kicks per minute", 60 / total_number_of_kicks)
+
+            sleep(interval)
+            self.autoplay_audio("beep.mp3")
+            info_holder.empty()
+
+        self.total_kicks += 1
+        progress_holder.empty()
+
+
 st.set_page_config(page_title="Countdown Timer", layout="wide")
+st.image("image.jpeg")
 
 stage_data = {
         "100":6,
@@ -41,6 +70,7 @@ total_kicks = 0
 init_page_countdown = st.empty()
 _, center_col, _ = init_page_countdown.columns([1,2,1])
 
+st.image("image.jpeg")
 center_col.subheader("Kick Evaluation platform")
 
 holder = center_col.empty()
@@ -60,7 +90,9 @@ if start_button:
 
         ph.empty() 
 
-        autoplay_audio("go.mp3")
+        # autoplay_audio("go.mp3")
 
+        for stage_idx, mapping in enumerate(stage_data.items()):
+            play_stage(stage_idx=stage_idx ,stage_period=mapping[0], total_number_of_kicks=mapping[1])
 
-
+        autoplay_audio("finish.mp3")
