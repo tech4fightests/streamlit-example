97440b5eced2adbf6d3b02ede1e2b412314dbf33
Caio C.R. Garcez <caio.garcez@redes.unb.br>
2024-02-27 17:03:27 -0300

    add counters

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,47 +6,11 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-        md = f"""
-            <audio autoplay controls style="display:none;">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-            </audio>
-            """
-        element = st.markdown(md, unsafe_allow_html=True)
-        return element
-        
-def play_stage(self, stage_idx: int, stage_period: str, total_number_of_kicks: int):
-    interval =  float(stage_period) / total_number_of_kicks
-
-    for i in range(total_number_of_kicks + 1):
-        info_holder = st.empty()
-        progress_holder = st.empty()
-
-        stage_holder, kick_holder, overall_kick_holder, kicks_per_minute_holder  = info_holder.columns([1,1,1,1])
-
-        progress = progress_holder.progress(0)
-        progress.progress(i * (1 / total_number_of_kicks))
-
-        if i < total_number_of_kicks:
-            stage_holder.metric("Stage", stage_idx)
-            kick_holder.metric("Kick", i)
-            overall_kick_holder.metric("Total Amount of Kicks", self.total_kicks)
-            kicks_per_minute_holder.metric("Kicks per minute", 60 / total_number_of_kicks)
-
-            sleep(interval)
-            self.autoplay_audio("beep.mp3")
-            info_holder.empty()
-
-        self.total_kicks += 1
-        progress_holder.empty()
-
-
 st.set_page_config(page_title="Countdown Timer", layout="wide")
-st.image("image.jpeg")
+
+_, col_img, _ = st.columns([1,2,1])
+
+col_img.image("image.jpeg", width=400)
 
 stage_data = {
         "100":6,
@@ -68,9 +32,48 @@ stage_data = {
 total_kicks = 0
 
 init_page_countdown = st.empty()
+
+
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio autoplay controls style="display:none;">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        element = st.markdown(md, unsafe_allow_html=True)
+        return element
+        
+def play_stage(stage_idx: int, stage_period: str, total_number_of_kicks: int, total_kicks: int):
+    interval =  float(stage_period) / total_number_of_kicks
+
+    for i in range(total_number_of_kicks + 1):
+        info_holder = st.empty()
+        progress_holder = st.empty()
+
+        stage_holder, kick_holder, overall_kick_holder, kicks_per_minute_holder  = info_holder.columns([1,1,1,1])
+
+        progress = progress_holder.progress(0)
+        progress.progress(i * (1 / total_number_of_kicks))
+
+        if i < total_number_of_kicks:
+            stage_holder.metric("Stage", stage_idx)
+            kick_holder.metric("Kick", i)
+            overall_kick_holder.metric("Total Amount of Kicks", total_kicks)
+            kicks_per_minute_holder.metric("Kicks per minute", 60 / total_number_of_kicks)
+
+            sleep(interval)
+            autoplay_audio("beep.mp3")
+            info_holder.empty()
+
+        total_kicks += 1
+        progress_holder.empty()
+
+
 _, center_col, _ = init_page_countdown.columns([1,2,1])
 
-st.image("image.jpeg")
 center_col.subheader("Kick Evaluation platform")
 
 holder = center_col.empty()
@@ -93,6 +96,6 @@ if start_button:
         # autoplay_audio("go.mp3")
 
         for stage_idx, mapping in enumerate(stage_data.items()):
-            play_stage(stage_idx=stage_idx ,stage_period=mapping[0], total_number_of_kicks=mapping[1])
+            play_stage(stage_idx=stage_idx ,stage_period=mapping[0], total_number_of_kicks=mapping[1], total_kicks=total_kicks)
 
         autoplay_audio("finish.mp3")
