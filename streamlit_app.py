6e4922e25163d1450199bc7cea2210ae0c1c5aac
Caio C.R. Garcez <caio.garcez@redes.unb.br>
2024-02-28 06:40:08 -0300

    add images and footer

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Countdown Timer", layout="wide")
 
 _, col_img, _ = st.columns([1,2,1])
 
-col_img.image("image.jpeg", width=400)
+col_img.image("image2.jpeg", width=350)
 
 stage_data = {
         "100":6,
@@ -29,10 +29,16 @@ stage_data = {
         "64.1":58,
         "63.9":62
         }
-total_kicks = 0
+
+if 'kicks' not in st.session_state:
+    st.session_state['kicks'] = 0
+
 
 init_page_countdown = st.empty()
 
+def get_image_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode()
 
 def autoplay_audio(file_path: str):
     with open(file_path, "rb") as f:
@@ -46,7 +52,7 @@ def autoplay_audio(file_path: str):
         element = st.markdown(md, unsafe_allow_html=True)
         return element
         
-def play_stage(stage_idx: int, stage_period: str, total_number_of_kicks: int, total_kicks: int):
+def play_stage(stage_idx: int, stage_period: str, total_number_of_kicks: int):
     interval =  float(stage_period) / total_number_of_kicks
 
     for i in range(total_number_of_kicks + 1):
@@ -61,20 +67,20 @@ def play_stage(stage_idx: int, stage_period: str, total_number_of_kicks: int, to
         if i < total_number_of_kicks:
             stage_holder.metric("Stage", stage_idx)
             kick_holder.metric("Kick", i)
-            overall_kick_holder.metric("Total Amount of Kicks", total_kicks)
-            kicks_per_minute_holder.metric("Kicks per minute", 60 / total_number_of_kicks)
+            overall_kick_holder.metric("Total Amount of Kicks", st.session_state["kicks"])
+            kicks_per_minute_holder.metric("Kicks per minute", format(60 / total_number_of_kicks, ".2f"))
 
             sleep(interval)
+            
             autoplay_audio("beep.mp3")
             info_holder.empty()
 
-        total_kicks += 1
+        st.session_state["kicks"] = st.session_state["kicks"] + 1
+
         progress_holder.empty()
 
 
 _, center_col, _ = init_page_countdown.columns([1,2,1])
-
-center_col.subheader("Kick Evaluation platform")
 
 holder = center_col.empty()
 start_button = holder.button('Start')
@@ -96,6 +102,51 @@ if start_button:
         # autoplay_audio("go.mp3")
 
         for stage_idx, mapping in enumerate(stage_data.items()):
-            play_stage(stage_idx=stage_idx ,stage_period=mapping[0], total_number_of_kicks=mapping[1], total_kicks=total_kicks)
+            play_stage(stage_idx=stage_idx ,stage_period=mapping[0], total_number_of_kicks=mapping[1])
 
         autoplay_audio("finish.mp3")
+
+image1_base64 = get_image_base64("image1.jpeg")
+image2_base64 = get_image_base64("image.jpeg")
+
+footer = f"""<style>
+a:link , a:visited{{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}}
+
+a:hover,  a:active {{
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}}
+
+.footer {{
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}}
+
+/* Style for image containers */
+.img-container {{
+display: inline-block;
+padding: 10px; /* Padding around images */
+}}
+</style>
+<div class="footer">
+<p>Developed By<p>
+<div class="img-container">
+<img src="data:image/jpeg;base64,{image1_base64}" style="width: 200px;" alt="Description of Image 1">
+</div>
+<div class="img-container">
+<img src="data:image/jpeg;base64,{image2_base64}" style="width: 200px;" alt="Description of Image 2">
+</div>
+</div>
+"""
+
+st.markdown(footer, unsafe_allow_html=True)
